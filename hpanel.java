b02f9f38b65bd23356e2d4fb125968045927e376
JaynaOng <130682850+JaynaOng@users.noreply.github.com>
2024-01-23 22:39:02 -0500

    help screens

--- hpanel.java
+++ hpanel.java
@@ -13,17 +13,26 @@ public class hpanel extends JPanel{
 	BufferedImage imghit = null;
 	BufferedImage imgmiss = null;
 	
-	BufferedImage imgboat2 = null;
-	BufferedImage imgboat3a = null;
-	BufferedImage imgboat3b = null;
-	BufferedImage imgboat4 = null;
-	BufferedImage imgboat5 = null;
+	BufferedImage img2boata = null;
+	BufferedImage img2boatb = null;
+	BufferedImage img3boat = null;
+	BufferedImage img4boat = null;
+	BufferedImage img5boat = null;
 	
+	BufferedImage imghelp3 = null;
+	BufferedImage imghelp2 = null;
+	//Variables
 	int intPanel;
 	
 	//Methods
 	public void paintComponent(Graphics g){
 		//Properties
+		g.setColor(Color.WHITE);
+		
+		g.fillRect(1005,66,238,524);
+		g.fillRect(1005,607,238,48);
+		g.fillRect(554,85,156,24);
+		g.fillRect(743,85,156,24);
 		String strMap[][] = new String[10][10];
 		String strLines = "";
 		String strSplits[];
@@ -95,20 +104,43 @@ public class hpanel extends JPanel{
 		if(intPanel == 1){
 			
 		}else if(intPanel == 2){
+			g.drawImage(img2boatb, 167, 270, null);
+			g.drawImage(img3boat, 347, 180, null);
+			g.drawImage(img3boat, 77, 450, null);
+			g.drawImage(img4boat, 437, 270, null);
+			g.drawImage(img5boat, 122, 405, null);
+			g.drawImage(imghelp2,0,0,null);
+		}else if(intPanel == 3){
+			g.drawImage(img2boatb, 167, 270, null);
+			g.drawImage(img3boat, 347, 180, null);
+			g.drawImage(img3boat, 77, 450, null);
+			g.drawImage(img4boat, 437, 270, null);
+			g.drawImage(img5boat, 122, 405, null);
+			
 			g.drawImage(imghit, 167, 270, null);
 			g.drawImage(imghit, 167, 315, null);
 			g.drawImage(imgmiss, 77, 360, null);
 			g.drawImage(imgmiss, 392, 225, null);
 			g.drawImage(imgmiss, 302, 450, null);
-		}else if(intPanel == 3){
 			
+			g.drawImage(imghit, 592, 495, null);
+			g.drawImage(imgmiss, 682, 225, null);
+			g.drawImage(imgmiss, 502, 180, null);
+			g.drawImage(imgmiss, 907, 315, null);
+			g.drawImage(imgmiss, 772, 405, null);
+			
+			g.fillRect(646,607,163,47);
+			g.drawImage(imghelp3,0,0,null);
 		}	
 	}
 	
 	//constructor
 	public hpanel(){
+		
+		
 		InputStream imageclass = null;
 		
+		//Water
 		imageclass = this.getClass().getResourceAsStream("Resources/watertile.png");
 		if(imageclass != null){
 			try{
@@ -125,6 +157,7 @@ public class hpanel extends JPanel{
 			}
 		}
 
+		//Hit
 		imageclass = this.getClass().getResourceAsStream("Resources/hit.png");
 		if(imageclass != null){
 			try{
@@ -140,7 +173,7 @@ public class hpanel extends JPanel{
 				System.out.println("Unable to load images");
 			}
 		}
-		
+		//Miss
 		imageclass = this.getClass().getResourceAsStream("Resources/missed.png");
 		if(imageclass != null){
 			try{
@@ -156,6 +189,135 @@ public class hpanel extends JPanel{
 				System.out.println("Unable to load images");
 			}
 		}
-		
+		//Boat 2a
+		imageclass = this.getClass().getResourceAsStream("Resources/2boath.png");
+		if(imageclass != null){
+			try{
+				img2boata = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(img2boata == null){
+			try{
+				img2boata = ImageIO.read(new File("Resources/2boath.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		//Boat 2b
+		imageclass = this.getClass().getResourceAsStream("Resources/2boatv.png");
+		if(imageclass != null){
+			try{
+				img2boatb = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(img2boatb == null){
+			try{
+				img2boata = ImageIO.read(new File("Resources/2boatv.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		//Boat 3
+		imageclass = this.getClass().getResourceAsStream("Resources/3boatv.png");
+		if(imageclass != null){
+			try{
+				img3boat = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(img3boat == null){
+			try{
+				img3boat = ImageIO.read(new File("Resources/3boatv.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		//Boat 4
+		imageclass = this.getClass().getResourceAsStream("Resources/4boatv.png");
+		if(imageclass != null){
+			try{
+				img4boat = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(img4boat == null){
+			try{
+				img4boat = ImageIO.read(new File("Resources/4boatv.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		//Boat 5
+		imageclass = this.getClass().getResourceAsStream("Resources/5boath.png");
+		if(imageclass != null){
+			try{
+				img5boat = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(img5boat == null){
+			try{
+				img5boat = ImageIO.read(new File("Resources/5boath.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		/*
+		//Help Image 1
+		imageclass = this.getClass().getResourceAsStream("help2.png");
+		if(imageclass != null){
+			try{
+				imghelp2 = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(imghelp2 == null){
+			try{
+				imghelp2 = ImageIO.read(new File("help2.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		*/
+		//Help Image 2
+		imageclass = this.getClass().getResourceAsStream("Resources/help2.png");
+		if(imageclass != null){
+			try{
+				imghelp2 = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(imghelp2 == null){
+			try{
+				imghelp2 = ImageIO.read(new File("Resources/help2.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
+		//Help Image 3
+		imageclass = this.getClass().getResourceAsStream("Resources/help3.png");
+		if(imageclass != null){
+			try{
+				imghelp3 = ImageIO.read(imageclass);
+			}catch(IOException e){
+				System.out.println("Unable to load image from jar");
+			}
+		}
+		if(imghelp3 == null){
+			try{
+				imghelp3 = ImageIO.read(new File("Resources/help3.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load images");
+			}
+		}
 	}
 }
